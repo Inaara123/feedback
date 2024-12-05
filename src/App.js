9cf57260a61fbf12cf7a013db5831f699588e15c
SABEEL AHMED <sabeelahmed@SABEELs-MacBook-Air.local>
2024-12-05 18:00:27 +0530

    params captured with routert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
 import FeedbackForm from './feedbackForm';
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
 <FeedbackForm/>
+</BrowserRouter>
+
   );
 }
 
